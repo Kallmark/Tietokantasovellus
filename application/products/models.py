bb89eccd11492a74b198137b97a687a615215fb1
Kalle Markkanen <markkanen.kalle@gmail.com>
2018-11-25 22:20:15 +0200

    Muokattu tietokanta vastaamaan omaa sovellusta

--- application/products/models.py
+++ application/products/models.py
@@ -1,14 +1,16 @@
 from application import db
 
-class Task(db.Model):
+class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
     onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
-    done = db.Column(db.Boolean, nullable=False)
+    price = db.Column(db.Integer)
+    amount = db.Column(db.Integer)
 
     def __init__(self, name):
         self.name = name
-        self.done = False
+        self.price = 0
+        self.amount = 0
