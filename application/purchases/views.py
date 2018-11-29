f2eb484215549f63c59383e10295e6630e6bef09
Kalle Markkanen <markkanen.kalle@gmail.com>
2018-11-29 13:55:01 +0200

    Työstetty kehitys- ja tuotantoympäiristöjä, paranneltu ostoympäristöä

--- application/purchases/views.py
+++ application/purchases/views.py
@@ -2,28 +2,23 @@ from application import app, db
 from flask import redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 from application.purchases.models import Purchase
+from application.products.models import Product
 
 @app.route("/purchases/new/")
 @login_required
 def purchases_form():
-    return render_template("purchases/new.html")
+    return render_template("purchases/new.html", products = Product.query.all())
 
 @app.route("/purchases/", methods=["POST"])
 @login_required
 def purchases_create():
-
-    #toistaiseksi eri form
-    form = ProductForm(request.form)
-
-    if not form.validate():
-        return render_template("purchases/new.html", form = form)
     
-    t = Purchase(account_id = current_user.id, product_id = 0)
+    t = Purchase(account_id = current_user.id, product_id = request.form.get("purchase"))
 
     db.session().add(t)
     db.session().commit()
-  
 
+    return redirect(url_for("purchases_form"))
 
 
 
